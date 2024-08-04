6f3d8645e7dbb8e5a5bf1ab03a8bbceea5627d40
kana7777 <109659839+kana7777@users.noreply.github.com>
2024-08-04 23:28:59 +0800

    assd

--- allmax/src/main.js
+++ allmax/src/main.js
@@ -1,11 +1,10 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
-import './assets/all.css'
 
 const app = createApp(App)
 
