f6f0d904109b7fa5a18436a8c470996843f24c32
kana7777 <109659839+kana7777@users.noreply.github.com>
2024-08-20 02:21:01 +0800

    [FIX]PassDataViaProps

---
+++ allmax/src/components/OrderList.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { defineProps } from 'vue'
+defineProps(['order-list'])
+
+const itemSubtotal = (drink) =>{
+    return drink.quantity * drink.price
+}
+</script>
+<template>
+    <hr> 
+        <div class="container">
+            
+            <div class="row  d-flex justify-content-center">
+                <div class="col-md-6">
+                    <div v-if="!orderList.id" class="alert alert-secondary text-center" role="alert">
+                        尚未建立訂單
+                    </div>
+                    <div v-else class="card shadow-sm">
+                        <div class="card-body">
+                            <div class="text"> 訂單編號 {{ orderList.id}}</div>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">品項</th>
+                                        <th scope="col">甜度</th>
+                                        <th scope="col">冰塊</th>
+                                        <th scope="col">數量</th>
+                                        <th scope="col">小計</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="drink in orderList.cart" :key="drink.id">
+                                        <td>{{ drink.name}}</td>
+                                        <td>{{ drink.ice }}</td>
+                                        <td>{{ drink.sugar }}</td>
+                                        <td>{{ drink.quantity}}</td>
+                                        <td>{{ itemSubtotal(drink) }}</td>
+                                        
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div>
+                                備註: <span> {{ orderList.description}}</span>
+                            </div>
+                            <div class="text-end">
+                                <h5>總計: <span>{{ orderList.sum }} </span></h5>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        
+    
+</template>
+<style scoped>
+
+</style>
--- allmax/src/views/OrderDrink.vue
+++ allmax/src/views/OrderDrink.vue
@@ -106,49 +106,14 @@
             </div>
         </div>
         <!-- 送出訂單 -->
-        <hr>
-        <div v-if="!orderList.id" class="alert alert-secondary text-center" role="alert">
-            尚未建立訂單
-        </div>
-        <div v-else class="card shadow-sm">
-            <div class="card-body">
-                <div class="text"> 訂單編號 {{ orderList.id}}</div>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">品項</th>
-                            <th scope="col">甜度</th>
-                            <th scope="col">冰塊</th>
-                            <th scope="col">數量</th>
-                            <th scope="col">小計</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="drink in orderList.cart" :key="drink.id">
-                            <td>{{ drink.name}}</td>
-                            <td>{{ drink.ice }}</td>
-                            <td>{{ drink.sugar }}</td>
-                            <td>{{ drink.quantity}}</td>
-                            <td>{{ itemSubtotal(drink) }}</td>
-                            
-                        </tr>
-                    </tbody>
-                </table>
-                <div>
-                    備註: <span> {{ orderList.description}}</span>
-                </div>
-                <div class="text-end">
-                    <h5>總計: <span>{{ orderList.sum }} </span></h5>
-                </div>
-            </div>
-        </div>
+        <OrderList :order-list="orderList" />
     </div>
     
 
 </template>
 <script setup>
 import { ref,computed } from "vue";
-
+import OrderList from '../components/OrderList.vue'
 const drinkData = [
   {
     "id": 1,
@@ -287,9 +252,7 @@ const createOrder = () => {
 };
 
 //
-const itemSubtotal = (drink) =>{
-    return drink.quantity * drink.price
-}
+
 
 </script>
 <style scoped>
